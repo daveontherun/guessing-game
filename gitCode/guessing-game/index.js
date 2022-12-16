6c7fc8da83bb40f81c7f4fb9e07ca820f3106eb1
John Madrid <jmadr37@gmail.com>
2022-12-15 21:00:51 -0500

    Added in some code I wrote to the bottom of the js. Probably really convoluting this main branch, so feel free to delete it if you want to, I have a copy of it saved locally

--- gitCode/guessing-game/index.js
+++ gitCode/guessing-game/index.js
@@ -3,66 +3,66 @@
 guess. 
 - Store this random number as a variable: something like winningNumber
 */
-let winNum = Math.floor((100 * Math.random()) + 1);
-console.log(winNum);
+let winNum = Math.floor(100 * Math.random() + 1);
 
-/*
-User querySelectorAll to return a node list (array) 
-of guesses. Loop through this array and push in the 
-user guesses
-*/
-// let arr = document.querySelectorAll('#guess');
+const usrNum = document.querySelector("#numIn");
 
-console.log(arr)
+//initialize array and reminaing number of attempts
+let attemptsArr = [];
+let maxAtt = 5;
+let attRemain
+ = maxAtt - attemptsArr.length;
 
-//user number prompt
-const usrGuess = document.querySelector("#numIN");
-const guessButton = document.querySelector("#guessBtn");
-guessArr = []
-let guessRemain = 5
-lowGuess = `Incorrect. You have ${guessRemain} remaining. The winning number is higher`
-highGuess = `Incorrect. You have ${guessRemain} remaining. The winning number is lower`
-function guessNum(usrGuess, winNum) {
-  if (0 >= usrGuess || usrGuess > 100) {
-    return "Answer needs to be between 1-100"
-  }
-  while (guessRemain > 0) {
-    if (usrGuess === winNum) {
-      return "YOU WIN!";
-    } else {
-        guessRemain--;
-        if (usrGuess > winNum) {
-        return highGuess;
-        } else {
-        return lowGuess;
-        
-      }
+// messages displayed when theguess is low or high
+low = `Incorrect. You have ${attRemain
+} remaining. The winning number is higher`;
+high = `Incorrect. You have ${attRemain
+} remaining. The winning number is lower`;
+
+function guessNum(usrNum, winNum) {
+  //check if user's guess is valid
+  if (0 >= usrNum || usrNum > 100) {
+    return "Answer needs to be between 1-100";
+  } //check if user's guess is correct
+  if (usrNum === winNum) {
+    return "YOU WIN!";
+  } //check if user has remaining attempts
+  if (attRemain
+     > 0) {
+    let attempts = document.querySelectorAll(".attempts");
+    let chance = document.querySelectorAll(".guess");
+
+    //loop through attempts
+    for (let i = 0; i < attemptsArr.length; i++) {
+      chance[i].textContent = attempts[i];
     }
-  } return `YOU LOSE! The winning number was ${winNum}.`;
+    //add to list of attempts and subtract from remaining attempts
+    console.log(attemptsArr.push(usrNum))
+    attRemain--;
+    console.log(attRemain
+      , attemptsArr)
+    if (usrNum > winNum) {
+      return high;
+    } else {
+      return low;
+    
+    }
+  }
+  console.log(attRemain
+    , attemptsArr);
+  //return once user runs out of attempts they have lost
+  return `YOU LOSE! The winning number was ${winNum}.`;
 }
 
 //Button functionality
+const guessButton = document.querySelector("#guessBtn");
+guessButton.addEventListener("click", () => {
+  const userGV = usrNum.value;
+  const result = guessNum(userGV, winNum);
+  console.log(result);
+  //Use function written to compare guessing with winNum
+});
 
-guessButton.addEventListener('click', () => {
-    const userGV = usrGuess.value;
-    const result = guessNum(userGV, winNum);
-    console.log(result);
-  //Use function written to compare guessing with winNum 
-})
-console.log(guessNum(8, 10));
-console.log(guessNum(-3, 10));
-console.log(guessNum(-1, 101));
-console.log(guessNum(101, 10));
-console.log(guessNum(-3, -3));
-console.log(guessNum(-1, 0));
-console.log(guessNum(101, 101));
-console.log(guessNum(0, 0));
-
-console.log(guessNum(3, 3));
-console.log(guessNum(1, 1));
-console.log(guessNum(100, 100));
-console.log(guessNum(99, 99));
-console.log(guessNum(50, 50));
 /*
 Users guesses (Maybe use an array with the length of 5 to store the
 guesses)
@@ -72,68 +72,71 @@ guesses)
 let resetButton = document.querySelector("#resetButton");
 
 // resetButton.addEventListener("click", () => {
-  /*
+/*
   Have this function return the user to the main html page
   (refresh the page)
   */
 // })
 
-/* 
-COPY OF CODE I WROTE TANGENTIALLY FOR THE GUESSING GAME ACTIVITY
-Just a different way to skin a cat if you wanted to give it a look. 
-guessButton.addEventListener('click', () => {
-    let guess = Number(userInput.value);
-    let arrOfGuesses = document.querySelectorAll('.guess');
+/*
+Here's some code I was writing while we were working earlier. 
+Just a different way to skin a cat, in case you were interested in 
+taking a look. 
 
-    if (guess < 1 || guess > 100) {
-        document.querySelector('#title').innerHTML = "You must choose a number between 1 and 100!";
-    }
-    else if (guessCounter === 5) {
-        if (guess === winNum) {
-            document.querySelector('#title').innerHTML = `Wow, you got lucky! The answer was ${winNum}`
-        }
-        else {
-            document.querySelector('#title').innerHTML = `You have no more guesses! The answer was ${winNum}`
-        }
-    }
-    else if (winner === true) {
-        document.querySelector('#title').innerHTML = "You already won! Go touch some grass!";
+guessButton.addEventListener('click', () => {
+  let guess = Number(userInput.value);
+  let arrOfGuesses = document.querySelectorAll('.guess');
+
+  if (guess < 1 || guess > 100) {
+    document.querySelector('#title').innerHTML = "You must choose a number between 1 and 100!";
+  }
+  else if (guessCounter === 5) {
+    if (guess === winNum) {
+      document.querySelector('#title').innerHTML = `Wow, you got lucky! The answer was ${winNum}`
     }
     else {
-        while (guessCounter < 5) {
-            guessCounter += 1;
-            if (guess === winNum) {
-                document.querySelector('#title').innerHTML = "You guessed the right number!";
-                winner = true;
-                for (let i = 0; i < arrOfGuesses.length; i++) {
-                    if (arrOfGuesses[i].innerHTML === '') {
-                        arrOfGuesses[i].innerHTML = guess;
-                        break;
-                    }
-                }
-                break;
-            }
-            else if (guess > winNum) {
-                document.querySelector('#title').innerHTML = "You guessed too high! Guess a lower number!"
-                for (let i = 0; i < arrOfGuesses.length; i++) {
-                    if (arrOfGuesses[i].innerHTML === '') {
-                        arrOfGuesses[i].innerHTML = guess;
-                        break;
-                    }
-                }
-                break;
-            }
-            else {
-                document.querySelector('#title').innerHTML = "You guessed too low! Guess a higher number!"
-                for (let i = 0; i < arrOfGuesses.length; i++) {
-                    if (arrOfGuesses[i].innerHTML === '') {
-                        arrOfGuesses[i].innerHTML = guess;
-                        break;
-                    }
-                }
-                break;
-            }
-        }
+      document.querySelector('#title').innerHTML = `You have no more guesses! The answer was ${winNum}`
     }
+  }
+  else if (winner === true) {
+    document.querySelector('#title').innerHTML = "You already won! Go touch some grass!";
+  }
+  else {
+    while(guessCounter < 5) {
+      guessCounter +=1;
+      if (guess === winNum) {
+        document.querySelector('#title').innerHTML = "You guessed the right number!";
+        winner = true;
+        for (let i = 0; i< arrOfGuesses.length; i++) {
+          if (arrOfGuesses[i].innerHTML === '') {
+            arrOfGuesses[i].innerHTML = guess;
+            break;
+          }
+        }
+        break;
+      }
+      else if (guess > winNum) {
+        document.querySelector('#title').innerHTML = "You guessed too high! Guess a lower number!"
+        for (let i = 0; i < arrOfGuesses.length; i++) {
+          if (arrOfGuesses[i].innerHTML === '') {
+            arrOfGuesses[i].innerHTML = guess;
+            break;
+          }
+        }
+        break;
+      }
+      else {
+        document.querySelector('#title').innerHTML = "You guessed too low! Guess a higher number!"
+        for (let i = 0; i < arrOfGuesses.length; i++) {
+          if (arrOfGuesses[i].innerHTML === '') {
+            arrOfGuesses[i].innerHTML = guess;
+            break;
+          }
+        }
+        break;
+      }
+    }
+  }
 })
-/*
+
+*/
